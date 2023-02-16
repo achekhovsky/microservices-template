3ad0473248a7d57d2ddcef0f30a8472dc8fe9896
achekhovsky <ringrabbit@rambler.ru>
2023-02-16 07:29:00 -0500

    Simple spring cloud config server (kotlin), generated by spring boot initializr with actuator. Its compiled into layered jar file whose layers packaged into a docker image.

---
+++ configserver/build.gradle.kts
@@ -0,0 +1,61 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
+plugins {
+	id("org.springframework.boot") version "3.0.2"
+	id("io.spring.dependency-management") version "1.1.0"
+	kotlin("jvm") version "1.7.22"
+	kotlin("plugin.spring") version "1.7.22"
+	id("com.palantir.docker") version "0.34.0"
+}
+
+group = "com.microframe"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+	mavenCentral()
+}
+
+extra["springCloudVersion"] = "2022.0.1"
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.springframework.cloud:spring-cloud-config-server")
+	implementation("org.springframework.cloud:spring-cloud-starter-bootstrap:4.0.0")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+docker {
+	name = "${project.group}-${project.name}:${project.version}"
+	tag("microframeConfigServer", "${project.version}")
+	setDockerfile(file("Dockerfile"))
+	buildArgs(mapOf("JAR_FILE" to "/build/libs/${project.name}-$version.jar"))
+	copySpec.from("build/libs").into("build/libs")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "17"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+tasks.named<BootJar>("bootJar") {
+	layered {
+		enabled.set(true)
+		includeLayerTools.set(true)
+	}
+}
---
+++ configserver/src/test/kotlin/com/microframe/configserver/ConfigurationServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.microframe.configserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConfigurationServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
