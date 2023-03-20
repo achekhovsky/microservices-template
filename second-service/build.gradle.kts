b49b9a151d73c6030c1461d29e44b2666a0c8533
achekhovsky <ringrabbit@rambler.ru>
2023-03-20 15:43:15 -0400

    Added keycloak server and oidc to secondservice throught resource server JWT

--- second-service/build.gradle.kts
+++ second-service/build.gradle.kts
@@ -39,9 +39,22 @@ dependencies {
 //	implementation("io.github.resilience4j:resilience4j-retry:${resilience4jVersion}")
 	implementation("io.github.resilience4j:resilience4j-bulkhead:${resilience4jVersion}")
 //	implementation("io.github.resilience4j:resilience4j-timelimiter:${resilience4jVersion}")
+
+	implementation("org.springframework.boot:spring-boot-starter-security:3.0.2")
+	implementation("org.springframework.boot:spring-boot-starter-oauth2-client:3.0.2")
+	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server:3.0.2")
+	implementation("org.springframework.security:spring-security-oauth2-jose:6.0.2")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
+extra["keycloakVersion"] = "21.0.1"
+
+dependencyManagement {
+	imports {
+		mavenBom("org.keycloak.bom:keycloak-adapter-bom:${property("keycloakVersion")}")
+	}
+}
 
 docker {
 	name = "${project.group}-${project.name}:${project.version}"
---
+++ second-service/src/main/kotlin/com/microframe/second/configuration/SecurityConfig.kt
@@ -0,0 +1,59 @@
+package com.microframe.second.configuration
+
+import com.nimbusds.jose.proc.JWSAlgorithmFamilyJWSKeySelector
+import com.nimbusds.jose.proc.JWSKeySelector
+import com.nimbusds.jose.proc.SecurityContext
+import com.nimbusds.jwt.proc.DefaultJWTProcessor
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.core.session.SessionRegistryImpl
+import org.springframework.security.oauth2.jwt.JwtDecoder
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder
+import org.springframework.security.web.DefaultSecurityFilterChain
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy
+import java.net.URL
+
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+
+    @Value("\${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
+    private lateinit var jwkSetUri: String
+
+    @Bean
+    protected fun sessionAuthenticationStrategy(): SessionAuthenticationStrategy? {
+        return RegisterSessionAuthenticationStrategy(SessionRegistryImpl())
+    }
+
+    @Bean
+    @Throws(java.lang.Exception::class)
+    fun authenticationManager(http: HttpSecurity): AuthenticationManager? {
+        return http.getSharedObject(AuthenticationManagerBuilder::class.java)
+            .build()
+    }
+
+    @Bean
+    fun jwtDecoder(): JwtDecoder {
+        // makes a request to the JWK Set endpoint
+        val jwsKeySelector: JWSKeySelector<SecurityContext> = JWSAlgorithmFamilyJWSKeySelector.fromJWKSetURL<SecurityContext>(URL(jwkSetUri))
+        val jwtProcessor: DefaultJWTProcessor<SecurityContext> = DefaultJWTProcessor()
+        jwtProcessor.jwsKeySelector = jwsKeySelector
+        return NimbusJwtDecoder(jwtProcessor)
+    }
+
+    @Bean
+    @Throws(Exception::class)
+    fun filterChain(http: HttpSecurity): DefaultSecurityFilterChain? {
+        http
+            .authorizeHttpRequests().anyRequest().authenticated().and()
+            .oauth2ResourceServer().jwt()
+        return http.build()
+    }
+}
